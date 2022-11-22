b3f87f5ec067394dc55e3f917637dff8afdc67f2
airon37 <airon3737@gmail.com>
2022-11-22 09:56:10 +0100

    formulario 1 completado

--- sg_erte_Airon_Sanchez/Form1.cs
+++ sg_erte_Airon_Sanchez/Form1.cs
@@ -19,39 +19,100 @@ namespace sg_erte_Airon_Sanchez
 
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.Close();
+            DialogResult rs2 = MessageBox.Show("¿Desea cerrar la aplicacion?", "Confirmacion",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (rs2 == DialogResult.Yes)
+            {
+                this.Close();
+            }
         }
 
         private void informesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             AdministrarInformes f2 = new AdministrarInformes();
-            f2.MdiParent = this;
-            f2.Dock = DockStyle.Fill;
-            f2.Show();
+            if (this.MdiChildren.Length > 0)
+            {
+                DialogResult rs2 = MessageBox.Show("¿Desea cerrar la ventana actual?", "Confirmacion",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (rs2 == DialogResult.Yes)
+                {
+                    f2.MdiParent = this;
+                    f2.Dock = DockStyle.Fill;
+                    f2.Show();
+                }
+            }
+            else
+            {
+                f2.MdiParent = this;
+                f2.Dock = DockStyle.Fill;
+                f2.Show();
+            }
         }
 
         private void ertesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             administrarErtes f2 = new administrarErtes();
-            f2.MdiParent = this;
-            f2.Dock = DockStyle.Fill;
-            f2.Show();
+            if (this.MdiChildren.Length > 0)
+            {
+                DialogResult rs2 = MessageBox.Show("¿Desea cerrar la ventana actual?", "Confirmacion",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (rs2 == DialogResult.Yes)
+                {
+                    f2.MdiParent = this;
+                    f2.Dock = DockStyle.Fill;
+                    f2.Show();
+                }
+            }
+            else
+            {
+                f2.MdiParent = this;
+                f2.Dock = DockStyle.Fill;
+                f2.Show();
+            }
         }
 
         private void empresasToolStripMenuItem_Click(object sender, EventArgs e)
         {
             admin_Empresas f2 = new admin_Empresas();
-            f2.MdiParent = this;
-            f2.Dock = DockStyle.Fill;
-            f2.Show();
+            if (this.MdiChildren.Length > 0)
+            {
+                DialogResult rs2 = MessageBox.Show("¿Desea cerrar la ventana actual?", "Confirmacion",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (rs2 == DialogResult.Yes)
+                {
+                    f2.MdiParent = this;
+                    f2.Dock = DockStyle.Fill;
+                    f2.Show();
+                }
+            }
+            else
+            {
+                f2.MdiParent = this;
+                f2.Dock = DockStyle.Fill;
+                f2.Show();
+            }
         }
 
         private void empleadosToolStripMenuItem_Click(object sender, EventArgs e)
         {
             administrarEmpleados f2 = new administrarEmpleados();
-            f2.MdiParent = this;
-            f2.Dock = DockStyle.Fill;
-            f2.Show();
+            if (this.MdiChildren.Length > 0)
+            {
+                DialogResult rs2 = MessageBox.Show("¿Desea cerrar la ventana actual?", "Confirmacion",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (rs2 == DialogResult.Yes)
+                {
+                    f2.MdiParent = this;
+                    f2.Dock = DockStyle.Fill;
+                    f2.Show();
+                }
+            }
+            else
+            {
+                f2.MdiParent = this;
+                f2.Dock = DockStyle.Fill;
+                f2.Show();
+            }
         }
     }
 }
